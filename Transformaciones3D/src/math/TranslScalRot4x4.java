5f6cc8d00ea3f999bb6a9cfff806e1921ae12636
Miguel <miguelsosa2003@gmail.com>
2023-09-06 21:22:51 -0500

    Rotacion lista

--- Transformaciones3D/src/math/TranslScalRot4x4.java
+++ Transformaciones3D/src/math/TranslScalRot4x4.java
@@ -14,7 +14,7 @@ public class TranslScalRot4x4 extends Matrix4x4 {
     double centerX = 0;
     double centerY = 0;
     double centerZ = 0;
-    public static final boolean CENTER_TRANFORMS = true;
+    public static boolean CENTER_TRANFORMS = true;
 
     public TranslScalRot4x4() {
         super();
@@ -45,9 +45,9 @@ public class TranslScalRot4x4 extends Matrix4x4 {
         Matrix4x4 rotationZ = new RotationZ4x4(thetaZ);
 
         if(CENTER_TRANFORMS){
-            Matrix4x4 m2 = new Translation4x4(-dx, -dy, -dz);
+            Matrix4x4 m2 = new Translation4x4(-centerX, -centerY, -centerZ);
             Matrix4x4 m3 = new Scaling4x4(sx, sy, sz);
-            Matrix4x4 m4 = new Translation4x4(dx, dy, dz);
+            Matrix4x4 m4 = new Translation4x4(centerX, centerY, centerZ);
             scaling.matrix = Matrix4x4.times(m4, Matrix4x4.times(m3, m2)).matrix;
             Matrix4x4 m5 = new RotationX4x4(thetaX);
             rotationX.matrix = Matrix4x4.times(m4, Matrix4x4.times(m5, m2)).matrix;
